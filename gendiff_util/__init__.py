167715a2bede969378366c4e9291260f961469bd
Konstantin Pavlov <k.pavlov.reg@yandex.ru>
2023-03-24 12:31:46 +0300

    fix

--- gendiff_util/__init__.py
+++ gendiff_util/__init__.py
@@ -1,1 +1,5 @@
-from gendiff_util.scripts.gendiff import generate_diff
+from gendiff_util.scripts.gendiff import generate_diff as g_diff
+
+
+def generate_diff(path_to_file1, path_to_file2):
+    return g_diff(path_to_file1, path_to_file2)
