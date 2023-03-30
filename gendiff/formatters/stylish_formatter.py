c993c7a68a5f3847ac90d7c43ecc247e0db6fc7a
Konstantin Pavlov <k.pavlov.reg@yandex.ru>
2023-03-30 12:19:23 +0300

    Форматтер Stylish использует модуль parser.py чтобы привести вложенные словари к единому виду

--- gendiff/formatters/stylish_formatter.py
+++ gendiff/formatters/stylish_formatter.py
@@ -1,51 +1,47 @@
+from gendiff.parser import parse_data
+
+
 def get_stylish_output(diff):
 
-    def walk_list(arg_list, offset=0):
+    def walk_list(arg_list, level=0):
         replacer = '  '
 
         def walk_dict(arg_dict):
             children = arg_dict.get('children')
+            status = arg_dict.get('status', '  ')
+            offset = (replacer * 2) * level
             if children:
-                return f"{replacer * (offset + 2)}{arg_dict['key']}: " + f"{walk_list(children, offset + 2)}"
-            else:
-                value = transform_dict(arg_dict.get('value'), offset, replacer)
-                value1 = transform_dict(arg_dict.get('value1'), offset, replacer)
-                value2 = transform_dict(arg_dict.get('value2'), offset, replacer)
-                status = arg_dict.get('status')
-                if status == 'removed':
-                    return f"{replacer * (offset + 1)}- {arg_dict['key']}: {value1}"
-                elif status == 'added':
-                    return f"{replacer * (offset + 1)}+ {arg_dict['key']}: {value2}"
-                elif status == 'updated':
-                    return [f"{replacer * (offset + 1)}- {arg_dict['key']}: {value1}",
-                            f"{replacer * (offset + 1)}+ {arg_dict['key']}: {value2}"]
-                elif status == 'without changes':
-                    return f"{replacer * (offset + 1)}  {arg_dict['key']}: {value}"
+                return f"{offset}{replacer + status}{arg_dict['key']}: " + f"{walk_list(children, level + 1)}"
+            value = transform_inner_dict(arg_dict.get('value'))
+            value1 = transform_inner_dict(arg_dict.get('value1'))
+            value2 = transform_inner_dict(arg_dict.get('value2'))
+            if status == 'removed':
+                return f"{offset}{replacer}- {arg_dict['key']}: {value1}"
+            elif status == 'added':
+                return f"{offset}{replacer}+ {arg_dict['key']}: {value2}"
+            elif status == 'updated':
+                return [f"{offset}{replacer}- {arg_dict['key']}: {value1}",
+                        f"{offset}{replacer}+ {arg_dict['key']}: {value2}"]
+            elif status == 'without changes':
+                return f"{offset}{replacer}  {arg_dict['key']}: {value}"
 
-        return '{\n' + '\n'.join(flatten(list(map(walk_dict, arg_list)))) + '\n' + f"{replacer * offset}" + '}'
+        def transform_inner_dict(value):
+            if isinstance(value, dict):
+                return walk_list(parse_data(value, value), level + 1)
+            else:
+                match value:
+                    case True | False:
+                        return str(value).lower()
+                    case value if value is None:
+                        return 'null'
+                    case _:
+                        return value
+
+        res_list = flatten(list(map(walk_dict, arg_list)))
+        result = '\n'.join(res_list)
+        return "{\n" + result + "\n" + f"{(replacer * 2) * level}" + "}"
 
     return walk_list(diff)
-
-
-def transform_dict(value, offset, replacer='  '):
-    if isinstance(value, dict):
-        result = ['{']
-        for key in value:
-            inner_value = value[key]
-            if isinstance(inner_value, dict):
-                result.append(
-                    f"{replacer * (offset + 4)}{key}: {transform_dict(inner_value, offset + 2)}")
-            else:
-                result.append(f"{replacer * (offset + 4)}{key}: {inner_value}")
-        result.append(f"{replacer * (offset + 2)}" + '}')
-        return '\n'.join(result)
-    match value:
-        case True | False:
-            return str(value).lower()
-        case value if value is None:
-            return 'null'
-        case _:
-            return value
 
 
 def flatten(tree):
